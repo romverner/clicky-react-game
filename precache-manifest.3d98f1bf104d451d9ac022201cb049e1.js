ff3528eeb9af1972b76de151643b28f946400fd2
Roman Verner <romverj@gmail.com>
2019-04-04 12:32:34 -0400

    Updates

---
+++ precache-manifest.3d98f1bf104d451d9ac022201cb049e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39738688316413d32d2037e5c3cbe0fc",
+    "url": "/react-game/static/media/back.39738688.jpg"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "4480c4c4c2b240abf271",
+    "url": "/react-game/static/js/main.f8b21a3d.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4480c4c4c2b240abf271",
+    "url": "/react-game/static/css/main.9a3e655c.chunk.css"
+  },
+  {
+    "revision": "b2073c975af8f7a370985be1130971f1",
+    "url": "/react-game/index.html"
+  }
+];
